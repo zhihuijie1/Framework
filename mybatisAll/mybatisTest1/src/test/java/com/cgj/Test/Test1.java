568bfc211293ddff27475814e445785835d45b94
yibai <404223879@qq.com>
2023-08-01 17:21:28 +0800

    mybatis

--- mybatisAll/mybatisTest1/src/test/java/com/cgj/Test/Test1.java
+++ mybatisAll/mybatisTest1/src/test/java/com/cgj/Test/Test1.java
@@ -5,14 +5,19 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class Test1 {
-    @Test
-    public void get() throws IOException {
+    private SqlSession sqlSession = null;
+
+    @Before
+    public void before() throws IOException {
         //SqlSessionFactoryBuilder是MyBatis框架中的一个辅助类，用于构建SqlSessionFactory。
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
         //使用Resources工具类从类路径中获取sqlMapConfig.xml配置文件的输入流。
@@ -21,13 +26,19 @@ public class Test1 {
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
         //通过sqlSessionFactory对象的openSession方法，创建一个SqlSession对象。
         //SqlSession是MyBatis的核心会话类，用于执行数据库操作，比如执行SQL语句、提交事务等。
-        SqlSession sqlSession=sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
+    }
 
+    @Test
+    public void get() throws IOException {
         List<Dept> findAll = sqlSession.selectList("findAll");
         for (Dept dept : findAll) {
             System.out.println(dept);
         }
+    }
 
+    @After
+    public void after() {
         //关闭SqlSession对象，释放数据库连接资源。在使用完SqlSession后，务必及时关闭，以防止资源泄漏。
         sqlSession.close();
     }
